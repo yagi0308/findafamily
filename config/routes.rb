a9bd451e59211f63a6d35932ceec16b472b82e94
yagi0308 <choodee@icloud.com>
2025-02-10 11:27:06 +0900

    ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
    root to: "posts#index"
-   
+   resources :users
    
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
