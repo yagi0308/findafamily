6488fe5366543065ab4c710a095488bb79a8ca85
yagi0308 <choodee@icloud.com>
2025-02-23 20:55:59 +0900

    修正実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_20_074354) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_23_113535) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -90,7 +90,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_20_074354) do
   end
 
   create_table "messages", charset: "utf8mb3", force: :cascade do |t|
-    t.string "message", collation: "utf8mb4_0900_ai_ci"
+    t.string "message"
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
     t.datetime "created_at", null: false
