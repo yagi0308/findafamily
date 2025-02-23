0972d72e74e63329dce4cf9069e6d0f9b3ae19ed
yagi0308 <choodee@icloud.com>
2025-02-23 11:43:47 +0900

    追加実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,13 @@
 class Message < ApplicationRecord
-  validates :message, presence: true, length: { maximum: 140 }
   belongs_to :user
   belongs_to :room
   has_one_attached :message_image
+
+  validates :message, presence: true, length: { maximum: 140 }, unless: :message_image_attached?
+
+  private
+
+  def message_image_attached?
+    message_image.attached?
+  end
 end
